63f6df1307a195cc80d401a0a0df9407b8cc225a
Shay <Shayvanpelt05@gmail.com>
2025-04-25 15:21:05 -0700

    .

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
   devIndicators: false,
-  ignoreDuringBuilds: true,
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // add other config options here
 };
-
 export default nextConfig;
